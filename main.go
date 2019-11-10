425e838324457ac68f9dedde58236665e652a1d4
neo <neo@example.ru>
2019-11-10 21:40:15 +0300

    templates

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+func indexHandle(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html", "templates/footer.html", "templates/header.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "index", nil)
+}
 
 func main() {
 	fmt.Println("Listening on port :5050")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>hello...</h1>")
-	})
+	http.HandleFunc("/", indexHandle)
 
 	http.ListenAndServe(":5050", nil)
 }
