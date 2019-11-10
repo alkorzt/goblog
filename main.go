fe9b1ad02cbbe27c485557d95a74483a34636929
neo <neo@example.ru>
2019-11-10 20:31:13 +0300

    list_and_serve_on_port

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Listening on port :5050")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello...\n")
+	})
+
+	http.ListenAndServe(":5050", nil)
 }
